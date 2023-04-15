1df55d237e0c724ecc96d6d593d02a48db72fe55
David Ortiz <dmortiz1@gmail.com>
2023-04-15 16:01:42 -0400

    Js

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+function updateTime() {
+    const date = new Date();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+    const time = `${hours}:${minutes}:${seconds}`;
+    const clock = document.getElementById('relog');
+    clock.textContent = time;
+  }
+  setInterval(updateTime, 1000);
